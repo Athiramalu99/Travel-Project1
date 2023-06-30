125dc6fe2e6a7d60027c05d662bb8dc227aa5f7f
Athiramalu99 <137681535+Athiramalu99@users.noreply.github.com>
2023-06-30 13:42:24 +0530

    file upload

---
+++ travelproject/credentials/views.py
@@ -0,0 +1,40 @@
+from django.shortcuts import render, redirect
+from django.contrib.auth.models import User
+from django.contrib import messages, auth
+
+
+# Create your views here.
+def login(request):
+    if request.method == 'POST':
+        username = request.POST['username']
+        password = request.POST['password']
+        user = auth.authenticate(username=username, password=password)
+        if user is not None:
+            auth.login(request, user)
+            return redirect('/')
+        else:
+            messages.info(request,"invalid credentials")
+            return redirect('login')
+
+    return render(request, "login.html")
+
+
+def register(request):
+    if request.method == 'POST':
+        username = request.POST['username']
+        password = request.POST['password']
+
+        if User.objects.filter(username=username).exists():
+            messages.info(request,"user taken")
+            return redirect("register")
+        else:
+            user = User.objects.create_user(username=username, password=password)
+            user.save();
+            print("User Created")
+            return redirect('login')
+    return render(request, "register.html")
+
+
+def logout(request):
+    auth.logout(request)
+    return redirect('/')
---
+++ travelproject/travelapp/views.py
@@ -0,0 +1,9 @@
+from django.shortcuts import render
+from .models import Places
+
+
+# Create your views here.
+def demo(request):
+    obj = Places.objects.all()
+
+    return render(request, "index.html", {'result': obj})
